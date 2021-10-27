ce7013b38b34c0d894217914ebda9df908ae4e69
Michal <mihal30060@gmail.com>
2021-10-27 14:07:22 +0200

    Spring Init

---
+++ lime-shop/src/main/java/com/lime/lime/shop/LimeShopApplication.java
@@ -0,0 +1,13 @@
+package com.lime.lime.shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LimeShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LimeShopApplication.class, args);
+	}
+
+}
---
+++ lime-shop/src/test/java/com/lime/lime/shop/LimeShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lime.lime.shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LimeShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
