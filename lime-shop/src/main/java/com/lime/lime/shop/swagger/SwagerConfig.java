1365bb4792fe4b70b129a23aa3300ed473bbd0e0
Michal <mihal30060@gmail.com>
2022-02-08 21:26:59 +0100

    add docker compose

--- lime-shop/src/main/java/com/lime/lime/shop/swagger/SwagerConfig.java
+++ lime-shop/src/main/java/com/lime/lime/shop/swagger/SwagerConfig.java
@@ -1,13 +1,10 @@
 package com.lime.lime.shop.swagger;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-
 import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiKey;
 import springfox.documentation.service.AuthorizationScope;
-
 import springfox.documentation.service.SecurityReference;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spi.service.contexts.SecurityContext;
@@ -17,8 +14,7 @@ import java.util.Arrays;
 import java.util.List;
 
 @Configuration
-public class SwaggerConfig {
-
+public class SwagerConfig {
     private ApiKey apiKey() {
         return new ApiKey("JWT", "Authorization", "header");
     }
